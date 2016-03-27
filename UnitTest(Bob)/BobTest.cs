790e7f1c89ff0f97d07a8aa675224da2f9f51724
kvnmach <kvnmach@gmail.com>
2016-03-27 18:38:44 -0500

    BobClass Created]

---
+++ UnitTest(Bob)/BobTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+
+namespace BobTest1
+{
+    public class Bob
+    {
+        public string Hey(string remark)
+        {
+            if (remark.Trim() == "")
+            {
+                return "Fine. Be that way!";
+            }
+            if (IsYelling(remark))
+            {
+                return "Whoa, chill out!";
+            }
+            if (remark.EndsWith("?"))
+            {
+                return "Sure.";
+            }
+
+            return "Whatever.";
+        }
+
+
+        public bool IsYelling(string input)
+        {
+            if (input.Any(c => char.IsDigit(c) && input.EndsWith("!")))
+            {
+                return true;
+            }
+
+            var input2 = input;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    input2 = input.Replace(input[i], '@');
+                }
+            }
+
+            if (input2 == input.ToUpper())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
